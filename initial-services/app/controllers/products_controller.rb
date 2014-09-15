29a043bbc29c0e82f564e830531d2c3df09137d5
Christopher Bartling <chris.bartling@gmail.com>
2014-09-15 10:37:08 -0500

    Only respond to JSON.

--- initial-services/app/controllers/products_controller.rb
+++ initial-services/app/controllers/products_controller.rb
@@ -1,12 +1,19 @@
 class ProductsController < ApplicationController
 
+  respond_to :json
+
   def index
+    payload = [
+      {product: {id: 126762736, name: 'Apple Watch'}},
+      {product: {id: 126762867, name: 'Apple AirPort Extreme'}},
+      {product: {id: 126762566, name: 'Apple AirPort Express'}},
+      {product: {id: 126762419, name: 'Apple iPod Touch 32GB'}},
+      {product: {id: 126762098, name: 'Apple iPhone 6 Plus'}},
+      {product: {id: 126762433, name: 'Apple iPhone 6'}},
+    ]
     respond_to do |format|
-      format.html {
-        render text: 'Why aren\'t you using the ReSTful API?'
-      }
       format.json {
-        render json: {product: {id: 126762736, name: 'Apple Watch'}}
+        render json: payload
       }
     end
   end
