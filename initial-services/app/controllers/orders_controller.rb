03dada9e58b8123f6bf0572d818a056cd1236f48
Christopher Bartling <chris.bartling@gmail.com>
2014-09-15 10:42:33 -0500

    Fixing orders.

--- initial-services/app/controllers/orders_controller.rb
+++ initial-services/app/controllers/orders_controller.rb
@@ -1,12 +1,18 @@
 class OrdersController < ApplicationController
 
+  respond_to :json
+
   def index
+    payload = [
+      {order: {id: 423767, order_number: 'A734E890', created_date: DateTime.now.to_s}},
+      {order: {id: 423768, order_number: 'A734E666', created_date: DateTime.now.to_s}},
+      {order: {id: 423769, order_number: 'A734E789', created_date: DateTime.now.to_s}},
+      {order: {id: 423770, order_number: 'A734E877', created_date: DateTime.now.to_s}},
+      {order: {id: 423771, order_number: 'A734E566', created_date: DateTime.now.to_s}}
+    ]
     respond_to do |format|
-      format.html {
-        render text: 'Why aren\'t you using the ReSTful API?'
-      }
       format.json {
-        render json: {order: {id: 423767, order_number: 'A734E890', created_date: DateTime.now.iso}}
+        render json: payload
       }
     end
   end
